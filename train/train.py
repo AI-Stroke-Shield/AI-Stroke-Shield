148fb5b64bd6c4d7bd0c693bcb28d8fe3dded3bb
Miss Phumzile <bettyphumzile739@gmail.com>
2025-09-26 14:59:23 +0200

    read csv dataset and  convert categorical data to numerical format

--- train/train.py
+++ train/train.py
@@ -9,3 +9,10 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 import joblib
+
+
+df = pd.read_csv('healthcare-dataset-stroke-data.csv') 
+
+
+df['bmi'] = pd.to_numeric(df['bmi'], errors='coerce')
+df['bmi'].fillna(df['bmi'].mean(), inplace=True)
