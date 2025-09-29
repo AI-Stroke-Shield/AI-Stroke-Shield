e07df5e96b39653caa4f7074e588b85914cb9e48
Mboniey <222217960@edu.vut.ac.za>
2025-09-29 14:19:20 +0200

    Evaluates model performance using accuracy, precision, recall, F1 score, and visualizes results with a confusion matrix.

---
+++ test/val.py
@@ -0,0 +1,12 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+y_test_loaded = np.load('y_test.npy')
+y_pred_loaded = np.load('y_pred.npy')
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Precision:", precision_score(y_test, y_pred))
+print("Recall:", recall_score(y_test, y_pred))
+print("F1 Score:", f1_score(y_test, y_pred))
