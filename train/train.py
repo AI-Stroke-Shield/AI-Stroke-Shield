dee4f719ebc3d6c85a8175cc3a69694d123ffa7b
Miss Phumzile <bettyphumzile739@gmail.com>
2025-09-30 14:59:41 +0200

    removed unused imports

--- train/train.py
+++ train/train.py
@@ -1,12 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from imblearn.pipeline import Pipeline as ImbPipeline
 from imblearn.over_sampling import SMOTE
 from xgboost import XGBClassifier
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 import joblib
 from sklearn.preprocessing import OrdinalEncoder
